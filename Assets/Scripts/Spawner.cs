aee256d1e29162df86a88b8a916e4a9ea8623e97
Rob Pilling <robpilling@gmail.com>
2023-08-02 21:13:01 +0100

    Spawner (no spawn entity)

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private const float SPAWN_PERIOD = 5.0f;
+    [SerializeField] private GameObject spawnPrefab;
+
+    private float cooldown = SPAWN_PERIOD;
+
+    void Update() {
+        cooldown -= Time.deltaTime;
+        if (cooldown > 0) return;
+        cooldown = SPAWN_PERIOD;
+
+        var spawnPos = transform.position;
+        spawnPos.x = Random.Range(-6, 6);
+
+        GameObject newAI = Instantiate(spawnPrefab);
+
+        newAI.transform.position = spawnPos;
+    }
+}
