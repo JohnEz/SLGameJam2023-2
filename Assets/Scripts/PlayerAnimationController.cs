9131a59b3d68471563bb3bd4d826952b61787ab4
unknown <andrew_nomie@msn.com>
2023-08-02 20:17:43 +0100

    Add animations

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour {
+    private Animator animator;
+
+    [HideInInspector]
+    public Vector2 MoveInput;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        MoveInput = Vector2.zero;
+    }
+
+    private void Update() {
+        bool isUpPressed = MoveInput.y > 0;
+        bool isDownPressed = MoveInput.y < 0;
+
+        animator.SetBool("SlowPressed", isUpPressed);
+        animator.SetBool("FastPressed", isDownPressed);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     private Moveable Moveable;
+    private PlayerAnimationController animationController;
 
-    private void Start()
-    {
+    private void Start() {
         Moveable = GetComponent<Moveable>();
+        animationController = GetComponent<PlayerAnimationController>();
     }
 
-    private void Update()
-    {
+    private void Update() {
         var moveX = Input.GetAxisRaw("Horizontal");
         var moveY = Input.GetAxisRaw("Vertical");
 
         var movementInput = new Vector2(moveX, moveY).normalized;
         Moveable.Force = movementInput;
+        animationController.MoveInput = movementInput;
 
         //var mousePos = Input.mousePosition;
         //var mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePos);
